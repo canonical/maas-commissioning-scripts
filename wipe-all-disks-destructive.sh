88008b88579593135e4141c0efb083f897170783
craig bender <19372989+ThinGuy@users.noreply.github.com>
2021-04-13 10:58:15 -0700

    Fix for new maas script metadata requirements

--- wipe-all-disks-destructive.sh
+++ wipe-all-disks-destructive.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 # --- Start MAAS 1.0 script metadata ---
-# name: wipe-all-disks-destructive
-# title: Wipe All Disks
-# description: Run to wipe disks of sticky filesystem data
-# script_type: test
+# name: wipe-disks-destructive
+# title: Wipe Disks
+# description: Runs wipefs -af, sgdisk -Z and dd's the bootsector of every disk
+# script_type: commissioning
 # hardware_type: storage
-# destructive: true
-# parallel: disabled
-# packages: {apt: sg3-utils, sg3-utils-udev}
-# tags: storage zap wipefs
-# timeout: 00:05:00
+# destructive: True
 # --- End MAAS 1.0 script metadata ---
+
 find /dev -maxdepth 1 -type b -regextype "posix-extended" -regex '.*/(sd[a-z]$|nvme[0-9]n[0-9])$'|xargs -rn1 -P1 bash -c '
 [[ $(command -v wipefs) ]] && { wipefs -af ${0}; }
 [[ $(command -v sgdisk) ]] && { sgdisk -Z ${0}; }
